881ecaa7a9e0c525610be551e0fc84a559fcd687
Martijn Bos <maboc@pm.me>
2026-08-01 16:11:32 +0200

    Wat gespeel met pointers en integers en strings

--- tlisp.c
+++ tlisp.c
@@ -1,5 +1,26 @@
+#include "tlisp.h"
+
 int main(int argc, char ** argv){
-  printf("tlisp\n");
+  struct waarde_type * waarde;
+
+  waarde=malloc(sizeof(struct waarde_type));
+  waarde->s=NULL;
+  waarde->s=malloc(strlen("hallo daar"));
+  bzero(waarde->s, strlen("hallo daar"));
+  waarde->s=strncpy(waarde->s, "hallo daar", strlen("hallo daar"));
+
   
+  printf("tlisp\n");
+
+  waarde->i=10;
+
+  printf("waarde (integer) :%i\n", waarde->i);
+  printf("waarde (string)  :%s\n", waarde->s);
+
+  free(waarde->s);
+  free(waarde);
+
+  
+
   return 0;
 }
